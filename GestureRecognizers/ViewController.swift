27c7dd7100fdfdbb34a3bb65d32b1aabe7d45b1b
Bruce Wayne <flashgordon@Richards-MacBook-Pro.local>
2014-12-10 10:59:01 -0600

    Initial Commit

---
+++ GestureRecognizers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureRecognizers
+//
+//  Created by Bruce Wayne on 12/10/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
